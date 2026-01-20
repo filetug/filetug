d0a82f28fc143f29b9a164d3319126f74b7850e3
Alexander Trakhimenok <alex@trakhimenok.com>
2026-01-20 16:58:53 +0000

    fix: previewer_test.go increased test coverage

--- pkg/filetug/previewer_test.go
+++ pkg/filetug/previewer_test.go
@@ -1,6 +1,7 @@
 package filetug
 
 import (
+	"fmt"
 	"os"
 	"path/filepath"
 	"testing"
@@ -24,13 +25,34 @@ func TestPreviewer(t *testing.T) {
 		p.Draw(s)
 	})
 
+	t.Run("SetText", func(t *testing.T) {
+		p.SetText("test text")
+		assert.Contains(t, p.textView.GetText(false), "test text")
+	})
+
+	t.Run("SetErr", func(t *testing.T) {
+		p.SetErr(fmt.Errorf("test error"))
+		assert.Contains(t, p.textView.GetText(false), "test error")
+	})
+
+	t.Run("PreviewFile_DSStore_Valid", func(t *testing.T) {
+		tmpFile, _ := os.CreateTemp("", ".DS_Store")
+		defer func() {
+			_ = os.Remove(tmpFile.Name())
+		}()
+		// Minimal DSStore header: 4 bytes Magic, 4 bytes "Bud1"
+		// See https://en.wikipedia.org/wiki/.DS_Store
+		header := []byte{0x00, 0x00, 0x00, 0x01, 0x42, 0x75, 0x64, 0x31}
+		_ = os.WriteFile(tmpFile.Name(), header, 0644)
+		p.PreviewFile(".DS_Store", tmpFile.Name())
+	})
+
 	t.Run("FocusBlur", func(t *testing.T) {
 		nav.previewerFocusFunc()
 		nav.previewerBlurFunc()
-	})
-
-	t.Run("TextViewFocus", func(t *testing.T) {
-		// p.textView.GetFocusFunc()() // Not available
+		p.Focus(func(p tview.Primitive) {})
+		p.Blur()
+		p.textView.Focus(func(p tview.Primitive) {})
 	})
 
 	t.Run("PreviewFile_NotFound", func(t *testing.T) {
@@ -114,15 +136,32 @@ func TestPreviewer(t *testing.T) {
 		assert.Error(t, err)
 	})
 
-	t.Run("PreviewFile_Image", func(t *testing.T) {
+	t.Run("PreviewFile_Image_Meta", func(t *testing.T) {
 		tmpFile, _ := os.CreateTemp("", "test*.png")
 		defer func() {
 			_ = os.Remove(tmpFile.Name())
 		}()
-		// Just an empty file might not work if imageviewer checks for headers, but let's see
-		err := os.WriteFile(tmpFile.Name(), []byte("not an image but .png extension"), 0644)
-		assert.NoError(t, err)
+		// A 1x1 pixel PNG file (minimal valid PNG)
+		pngData := []byte{
+			0x89, 0x50, 0x4E, 0x47, 0x0D, 0x0A, 0x1A, 0x0A, 0x00, 0x00, 0x00, 0x0D, 0x49, 0x48, 0x44, 0x52,
+			0x00, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00, 0x01, 0x08, 0x02, 0x00, 0x00, 0x00, 0x90, 0x77, 0x53,
+			0xDE, 0x00, 0x00, 0x00, 0x0C, 0x49, 0x44, 0x41, 0x54, 0x08, 0xD7, 0x63, 0xF8, 0xFF, 0xFF, 0x3F,
+			0x00, 0x05, 0xFE, 0x02, 0xFE, 0xDC, 0x44, 0x74, 0x06, 0x00, 0x00, 0x00, 0x00, 0x49, 0x45, 0x4E,
+			0x44, 0xAE, 0x42, 0x60, 0x82,
+		}
+		_ = os.WriteFile(tmpFile.Name(), pngData, 0644)
+		p.PreviewFile(filepath.Base(tmpFile.Name()), tmpFile.Name())
+	})
 
+	t.Run("PreviewFile_ChromaError", func(t *testing.T) {
+		// To trigger chroma error, we can try something that is not valid UTF-8 if the lexer expects it
+		// but chroma2tcell usually handles bytes.
+		// However, it's worth a try with some invalid bytes for a specific lexer.
+		tmpFile, _ := os.CreateTemp("", "test*.go")
+		defer func() {
+			_ = os.Remove(tmpFile.Name())
+		}()
+		_ = os.WriteFile(tmpFile.Name(), []byte{0xff, 0xfe, 0xfd}, 0644)
 		p.PreviewFile(filepath.Base(tmpFile.Name()), tmpFile.Name())
 	})
 
@@ -138,16 +177,27 @@ func TestPreviewer(t *testing.T) {
 		assert.Contains(t, p.textView.GetText(false), "log line")
 	})
 
-	t.Run("PreviewFile_DSStore", func(t *testing.T) {
-		// Mocking a .DS_Store is hard, let's just try to call it with an empty file
-		tmpFile, _ := os.CreateTemp("", ".DS_Store")
+	t.Run("PreviewFile_DSStore_Error_ReadFile", func(t *testing.T) {
+		// To trigger readFile error inside DSStore branch
+		tmpDir, _ := os.MkdirTemp("", "testds")
 		defer func() {
-			_ = os.Remove(tmpFile.Name())
+			_ = os.RemoveAll(tmpDir)
 		}()
-		_ = os.WriteFile(tmpFile.Name(), []byte("invalid ds_store"), 0644)
+		dsPath := filepath.Join(tmpDir, ".DS_Store")
+		_ = os.Mkdir(dsPath, 0755) // Create as directory to cause read error
+		p.PreviewFile(".DS_Store", dsPath)
+	})
 
-		p.PreviewFile(".DS_Store", tmpFile.Name())
-		assert.Contains(t, p.textView.GetText(false), "invalid file header")
+	t.Run("readFileError", func(t *testing.T) {
+		// Test readFile error handling.
+		// We can try to read a directory as a file.
+		tmpDir, _ := os.MkdirTemp("", "testdir")
+		defer func() {
+			_ = os.RemoveAll(tmpDir)
+		}()
+		_, err := p.readFile(tmpDir, 0)
+		assert.Error(t, err)
+		assert.Contains(t, p.textView.GetText(false), "Error reading file")
 	})
 
 	t.Run("readFile", func(t *testing.T) {
